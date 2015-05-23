793aac646aae73264c2c89b8cbf1bfc7c7474b81
RoxasShadow <webmaster@giovannicapuano.net>
2015-05-23 17:16:27 +0200

    test_search_artists: update with new APIs

--- tests/test_search_artists.rs
+++ tests/test_search_artists.rs
@@ -6,7 +6,7 @@ use rustfm::Artist;
 #[test]
 pub fn search_artists() {
   let last_fm = LastFM::new("572b13444704f89c67b07a713d5e5de1");
-  let search  = last_fm.search_artists("nightwish");
+  let search  = Artist::search(last_fm, "nightwish");
 
   let artists : Vec<Artist> = search.results;
   assert!(!artists.is_empty());
