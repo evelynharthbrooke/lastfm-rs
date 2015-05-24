40824aa406db36907e939538910e6d55114998d9
RoxasShadow <webmaster@giovannicapuano.net>
2015-05-24 02:45:03 +0200

    test_artist_events: use nightwish as sample artist

--- tests/test_artist_events.rs
+++ tests/test_artist_events.rs
@@ -7,7 +7,7 @@ use rustfm::Event;
 #[test]
 pub fn search_artists() {
   let last_fm = LastFM::new("572b13444704f89c67b07a713d5e5de1");
-  let search  = Artist::events(last_fm, "sonata arctica");
+  let search  = Artist::events(last_fm, "nightwish");
 
   let events : Vec<Event> = search.results;
   assert!(!events.is_empty());
