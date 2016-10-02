2b65b93a6b4870920ab6dd377cd6b0c191897d2e
Giovanni Capuano <webmaster@giovannicapuano.net>
2016-10-02 13:38:08 +0200

    lib: make add_param! functions public

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ use hyper::client::Client as HTTPClient;
 
 macro_rules! add_param {
     ($f:ident, $p:ident, $t:ty) => {
-        fn $f(&'a mut self, v: $t) -> &'a mut Self {
+        pub fn $f(&'a mut self, v: $t) -> &'a mut Self {
             self.url.query_pairs_mut().append_pair(
                 stringify!($p), &*v.to_string());
             self
