05994120ca47f05d87cfcd01ee0358560fe095be
Giovanni Capuano <webmaster@giovannicapuano.net>
2016-10-22 02:52:11 +0200

    cargo: use serde_derive

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 #![feature(custom_derive, plugin)]
-#![plugin(serde_macros)]
+#![feature(proc_macro)]
 
+#[macro_use] extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 extern crate hyper;
 extern crate url;
-
 #[macro_use] extern crate lazy_static;
 
 use std::fmt;
